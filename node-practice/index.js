677648ae5b575ccf9b28eae8db9b7c91875fa653
Aditya Prakash <p.aditya.198@gmail.com>
2019-05-15 23:30:17 +0530

    changes in practice folder in index.js

--- node-practice/index.js
+++ node-practice/index.js
@@ -4,7 +4,7 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-
+    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>Hello World!</h1></body></html>');
