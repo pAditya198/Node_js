4123af6011f1054f15800d0eb76a9cd5ce69eaf3
Aditya Prakash <p.aditya.198@gmail.com>
2019-05-15 23:25:46 +0530

    changes in practice folder added json

--- node-practice/index.js
+++ node-practice/index.js
@@ -6,8 +6,8 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 
     res.statusCode = 200;
-    res.setHeader = ('Content-Type', 'text/html');
-    res.end(' < html > < body > < h1 >Hello World!< /h1></body > < /html>')
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Hello World!</h1></body></html>');
 
 
 });
