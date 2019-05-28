4699c8d34bb8ab40e3a941dfebd44ee9343c1291
Aditya Prakash <p.aditya.198@gmail.com>
2019-05-28 12:52:26 +0530

    Express generator REST API

---
+++ conFusionServer/routes/dishRouter.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+
+dishRouter.route('')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", 'text/plain');
+        next();
+    })
+
+
+    .get((req, res, next) => {
+        res.end('Will send  to you!');
+    })
+
+    .post((req, res, next) => {
+        res.end('Will add the dish : ' + req.body.name + ' with details : ' + req.body.description);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation supported for dishes');
+
+    })
+
+    .delete((req, res, next) => {
+        res.end('Deleting all the dishes');
+    });
+
+dishRouter.route('/:dishId').all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", 'text/plain');
+
+    next();
+
+}).get((req, res, next) => {
+    res.end('Will send details of the dishes : ' + req.params.dishId + ' to you !');
+}).post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation supported for /dishes/' + req.params.dishId);
+}).put((req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end("will Update the dish: " + req.body.name + ' with details ' +
+        req.body.description);
+}).delete((req, res, next) => {
+    res.end('Deleting the dish: ' + req.params.dishId);
+});
+
+
+module.exports = dishRouter;
