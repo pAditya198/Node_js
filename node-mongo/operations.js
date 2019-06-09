264044afced580bc569cff31f027070f57b4426b
Aditya Prakash <p.aditya.198@gmail.com>
2019-06-09 20:56:58 +0530

    Removed Callbacks from Operation.js

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -3,37 +3,20 @@ const assert = require('assert');
 exports.insertDocumnet = (db, document, collection, callback) => {
 
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted " + result.result.n + " documents into the " + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 
 };
 exports.findDocumnets = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, doc) => {
-        assert.equal(err, null);
-        callback(doc);
-    })
+    return coll.find({}).toArray();
 
 };
 exports.removeDocumnet = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result)
-    })
+    return coll.deleteOne(document);
 
 };
 exports.updateDocumnets = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, {
-        $set: update
-    }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result)
-    })
+    return coll.updateOne(document);
 };
