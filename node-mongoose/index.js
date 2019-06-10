dba1d404b6c022cb229e0c68c138202d8b69db83
Aditya Prakash <p.aditya.198@gmail.com>
2019-06-10 23:35:09 +0530

    Mongoose part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -16,10 +16,29 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: {
+                    description: "updated test"
+                }
+            }, {
+                new: true
+
+            }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+
+        })
+        .then((dish) => {
+            console.log(dish);
 
             return Dishes.remove({});
         })
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -2,6 +2,25 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
 const dishSchema = new Schema({
 
     name: {
@@ -13,7 +32,8 @@ const dishSchema = new Schema({
         type: String,
         required: true
 
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
