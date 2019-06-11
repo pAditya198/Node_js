c7e714d7bd8306b7e6628a35b39be9af8027ab8b
Aditya Prakash <p.aditya.198@gmail.com>
2019-06-11 22:53:00 +0530

    UPDATED dishes.js

--- conFusionServer/models/dishes.js
+++ conFusionServer/models/dishes.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
 
 const commentSchema = new Schema({
     rating: {
@@ -32,6 +35,28 @@ const dishSchema = new Schema({
         type: String,
         required: true
 
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0,
+
+    },
+    featured: {
+        type: Boolean,
+        default: false
     },
     comments: [commentSchema]
 }, {
