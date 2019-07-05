ae52abb9a7aa160003538b4b96a8bfaa4a15feb0
Aditya Prakash <p.aditya.198@gmail.com>
2019-07-05 23:06:08 +0530

    Assignment 3 completed

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -52,7 +52,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
